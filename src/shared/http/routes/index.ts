59bed503ba83e9a84819da53ed4aaa48723eb92e
MatheusTrinca <matheusfelipetrinca@gmail.com>
2022-09-15 21:17:28 -0300

    add errors middleware

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,11 @@
+import { AppError } from '@shared/errors/AppError'
 import { Router } from 'express'
 
 const routes = Router()
 
 routes.get('/', (request, response) => {
-  response.send({ message: 'Hello World' })
+  throw new AppError('Acesso negado')
+  // return response.send({ message: 'Hello World' })
 })
 
 export { routes }
