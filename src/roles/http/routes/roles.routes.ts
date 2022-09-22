6daa499e40af93a1e8b27064710b32fa1a4115d9
MatheusTrinca <matheusfelipetrinca@gmail.com>
2022-09-22 16:08:38 -0300

    show role

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -1,5 +1,6 @@
 import { createRolesController } from '@roles/useCases/createRole'
 import { listRolesController } from '@roles/useCases/listRoles'
+import { showRoleController } from '@roles/useCases/showRole'
 import { Router } from 'express'
 
 const rolesRouter = Router()
@@ -9,6 +10,10 @@ rolesRouter.post('/', (request, response) => {
 
 rolesRouter.get('/', (request, response) => {
   return listRolesController.handle(request, response)
+})
+
+rolesRouter.get('/:id', (request, response) => {
+  return showRoleController.handle(request, response)
 })
 
 export { rolesRouter }
