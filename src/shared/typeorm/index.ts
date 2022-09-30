bb613c6b50d5cac9f8d7a52fe3ccc5bc0943985b
MatheusTrinca <matheusfelipetrinca@gmail.com>
2022-09-30 03:52:41 -0300

    users entities

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,4 +1,5 @@
 import { Role } from '@roles/entities/Role'
+import { User } from '@users/entities/User'
 import { DataSource } from 'typeorm'
 import { CreateRolesTable1663378783400 } from './migrations/1663378783400-CreateRolesTable'
 import { CreateUsersTable1664221020631 } from './migrations/1664221020631-CreateUsersTable'
@@ -7,7 +8,7 @@ import { AddRoleIdToUsersTable1664221815210 } from './migrations/1664221815210-A
 export const datasource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
-  entities: [Role],
+  entities: [Role, User],
   migrations: [
     CreateRolesTable1663378783400,
     CreateUsersTable1664221020631,
